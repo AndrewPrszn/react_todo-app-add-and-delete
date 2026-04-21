7c601fbd386176922cce703d206419891ad28573
MacBook Pro <macbookpro@Andrew.local>
2026-04-21 19:12:08 +0300

    add task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 'andrew-todo-app';
+export const USER_ID = 4139;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
